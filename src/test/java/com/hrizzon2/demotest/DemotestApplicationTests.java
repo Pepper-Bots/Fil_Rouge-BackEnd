e3de9a38b4a91afc1e6278b89321051ec8911f17
Hélène Rizzon <helene.rizzon@live.fr>
2025-03-03 21:42:32 +0100

    Initial commit

---
+++ src/test/java/com/hrizzon2/demotest/DemotestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrizzon2.demotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemotestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
