b60b0c24920d7a2adf82dc427f99e723fa649c64
Hélène Rizzon <helene.rizzon@live.fr>
2025-04-09 16:23:02 +0200

    "Ajout d'une base de données test + gestion objet Dossier"

--- src/main/java/com/hrizzon2/demotest/DemotestApplication.java
+++ src/main/java/com/hrizzon2/demotest/DemotestApplication.java
@@ -1,13 +1,23 @@
 package com.hrizzon2.demotest;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class DemotestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemotestApplication.class, args);
-	}
+    public static void main(String[] args) {
 
+        SpringApplication.run(DemotestApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+
+    }
 }
+
