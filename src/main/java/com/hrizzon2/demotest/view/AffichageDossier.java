76abfd1d10339ade5b74e9cab9295b45474560ce
Hélène Rizzon <helene.rizzon@live.fr>
2025-04-25 12:05:33 +0200

    ajout AffichageDossier

--- src/main/java/com/hrizzon2/demotest/view/AffichageDossier.java
+++ src/main/java/com/hrizzon2/demotest/view/AffichageDossier.java
@@ -2,3 +2,10 @@ package com.hrizzon2.demotest.view;
 
 public class AffichageDossier {
 }
+
+// faire un heritage ici
+// -> affichage pour admin (voit et modifie tous les dossiers) != affichage pour stagiaire (voit et modifie uniquement son dossier)
+// créer 2 affichages dossier !=
+
+// a faire pour stagiaire aussi -> différent traitement -> se rappeler qu'un stagiaire peut avoir plusieurs dossiers d'inscription
+
